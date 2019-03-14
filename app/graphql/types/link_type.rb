08250160a262cb0f4d2859b4128c94968d4d98ef
radabaugh <radabaughtyler24@gmail.com>
2019-03-14 12:19:26 -0700

    Add posted_by field to link_type

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -3,6 +3,7 @@ module Types
     field :id, ID, null: false
     field :url, String, null: false
     field :description, String, null: false
+    field :posted_by, UserType, null: true, method: :user
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
