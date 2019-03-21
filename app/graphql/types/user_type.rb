94c5d331510fc581738ee49e231da25357a4365c
radabaugh <radabaughtyler24@gmail.com>
2019-03-21 10:47:17 -0700

    Relate users with their votes, #4

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -3,5 +3,7 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false # Exposing `email` just beacause it doesn't matter here. A real application shouldn't leak user emails 
+    field :votes, [VoteType], null: false
+    field :links, [LinkType], null: false
   end
 end
