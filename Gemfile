aac2e901a7c196cb60bbff0cd5689b1fc13ed49e
Tyler Radabaugh <radabaughtyler24@gmail.com>
2019-03-21 16:24:25 -0700

    Filtering (#10)
    * Add GraphQL SearchObject gem
    * Add Search Resolver
    * Add links_search_test
    * update query type resolver
    * Add scope to link for filtering
    * CreateLinkTest needed to have a current_user in the context block

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'graphql', '1.8.13'
 gem 'bcrypt', '~> 3.1.7'
+gem 'search_object', '1.2.0'
+gem 'search_object_graphql', '0.1'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- test/graphql/mutations/create_link_test.rb
+++ test/graphql/mutations/create_link_test.rb
@@ -1,14 +1,29 @@
 require 'test_helper'
 
 class Mutations::CreateLinkTest < ActiveSupport::TestCase
+  def perform_user(args = {})
+    Mutations::CreateUser.new(object: nil, context: {}).resolve(args)
+  end
+
   def perform(user: nil, **args)
-    Mutations::CreateLink.new(object: nil, context: {}).resolve(args)
+    Mutations::CreateLink.new(object: nil, context: {:current_user => user}).resolve(args)
   end
 
   test 'create a new link' do
+    user = perform_user(
+      name: 'Test User',
+      auth_provider: {
+        email: {
+          email: 'email@example.com',
+          password: '[omitted]'
+        }
+      }
+    )
+
     link = perform(
       url: 'http://example.com',
       description: 'description',
+      user: user
     )
 
     assert link.persisted?
